109658d08d87d0d0204c08749fb1ef2fde639224
YJH2848 <yunj05763@gmail.com>
2022-12-25 19:23:54 +0900

    design

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { useForm } from "react-hook-form";
+import * as S from "./style/style";
 
 export default function App() {
   const {
@@ -9,41 +10,45 @@ export default function App() {
   const onSubmit = data => console.log(data);
 
   return (
-    <div className="App">
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <input
-          name="email"
-          placeholder="이메일"
-          {...register("email", {
-            required: "이메일을 입력해주세요",
-            pattern: {
-              value: /\S+@\S+\.\S+/,
-              message: "이메일 형식에 맞게 입력을 해주세요",
-            },
-          })}
-        />
-        {errors.email && <p>{errors.email.message}</p>}
-        <input
-          type="password"
-          name="password"
-          placeholder="비밀번호"
-          {...register("password", {
-            required: "비밀번호를 입력해주세요",
-            minLength: {
-              value: 6,
-              message: "최소 6자 이상의 비밀번호를 입력해주세요",
-            },
-          })}
-        />
-        {errors.password && <p>{errors.password.message}</p>}
-        <input
-          type="password"
-          name="passwordConfirm"
-          placeholder="비밀번호 확인"
-          {...register("passwordConfirm")}
-        />
-        <input type="submit" />
-      </form>
-    </div>
+    <S.Container className="App">
+      <S.Box>
+        <S.Form onSubmit={handleSubmit(onSubmit)}>
+          <S.Input
+            name="email"
+            placeholder="이메일"
+            {...register("email", {
+              required: "이메일을 입력해주세요",
+              pattern: {
+                value: /\S+@\S+\.\S+/,
+                message: "이메일 형식에 맞게 입력을 해주세요",
+              },
+            })}
+          />
+          {errors.email && <S.Err>{errors.email.message}</S.Err>}
+
+          <S.Input
+            type="password"
+            name="password"
+            placeholder="비밀번호"
+            {...register("password", {
+              required: "비밀번호를 입력해주세요",
+              minLength: {
+                value: 6,
+                message: "최소 6자 이상의 비밀번호를 입력해주세요",
+              },
+            })}
+          />
+          {errors.password && <S.Err>{errors.password.message}</S.Err>}
+
+          <S.Input
+            type="password"
+            name="passwordConfirm"
+            placeholder="비밀번호 확인"
+            {...register("passwordConfirm")}
+          />
+          <S.Input type="submit" />
+        </S.Form>
+      </S.Box>
+    </S.Container>
   );
 }
---
+++ src/style/style.js
@@ -0,0 +1,34 @@
+import styled from "styled-components";
+
+export const Input = styled.input`
+  width: 20vw;
+  height: 5vh;
+  margin: 20px;
+`;
+
+export const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+`;
+
+export const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+`;
+
+export const Err = styled.div`
+  display: flex;
+  justify-content: center;
+  color: red;
+`;
+
+export const Box = styled.div`
+  border: 2px solid black;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 70vh;
+  width: 30vw;
+`;
